e50e96bc2cbf30a69ffe98e5082564699558a030
gmalatesta <giada.malatesta@cnaf.infn.it>
2023-11-29 16:32:09 +0100

    Update fio tests

--- run-fio-rand-readwrite.sh
+++ run-fio-rand-readwrite.sh
@@ -9,9 +9,9 @@ FILE_SIZE=$2
 BLOCK_SIZE=$3
 for count in $(seq 1 3)
 do
-  mkdir -p $1_clients/random/$count
+  mkdir -p /sts-wire-scale-tests/fio_output/$1_clients/random/$count
   for ((index=0; index != $(($CLI_NUM * 4)); index=$(($index + 4)))); do
-    wassh -f client-hosts -l root 'CLI_INDEX=$(expr '$(($index + 1))' + $CLIENT_X_FACTOR) && docker exec sts-wire-client$CLI_INDEX sh -c "cd /home/docker/rgw && rm -f test-4m.0 && fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test-4m --filename=test-4m.0 --bs="'$BLOCK_SIZE'" --iodepth=64 --size="'$FILE_SIZE'" --readwrite=randrw --rwmixread=75"' > $1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" 2>&1 & pids+=($!)
+    wassh -f client-hosts -l root 'CLI_INDEX=$(expr '$(($index + 1))' + $CLIENT_X_FACTOR) && docker exec sts-wire-client$CLI_INDEX sh -c "cd /home/docker/rgw && rm -f test-4m.0 && fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test-4m --filename=test-4m.0 --bs="'$BLOCK_SIZE'" --iodepth=64 --size="'$FILE_SIZE'" --readwrite=randrw --rwmixread=75"' > /sts-wire-scale-tests/fio_output/$1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" 2>&1 & pids+=($!)
   done
   for pid in "${pids[@]}"; do
     wait "$pid"
@@ -19,8 +19,8 @@ do
   sleep 180
   pids=()
   for ((index=0; index != $(($CLI_NUM * 4)); index=$(($index + 4)))); do
-    grep read: $1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" | awk '{gsub(/[IOPS=,(MB/s]/," "); print $2, $6}' > $1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-read.log" 2>&1
-    grep write: $1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" | awk '{gsub(/[IOPS=,(MB/s]/," "); print $2, $6}' > $1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-write.log" 2>&1
+    grep read: /sts-wire-scale-tests/fio_output/$1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" | awk '{gsub(/[IOPS=,(MB/s]/," "); print $2, $6}' > /sts-wire-scale-tests/fio_output/$1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-read.log" 2>&1
+    grep write: /sts-wire-scale-tests/fio_output/$1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-readwrite.log" | awk '{gsub(/[IOPS=,(MB/s]/," "); print $2, $6}' > /sts-wire-scale-tests/fio_output/$1_clients/random/$count/"sts-wire-client"$(($index + 1))"-rand-write.log" 2>&1
     wassh -f client-hosts -l root 'CLI_INDEX=$(expr '$(($index + 1))' + $CLIENT_X_FACTOR) && docker exec sts-wire-client$CLI_INDEX sh -c "cd /home/docker/rgw && rm -f test-4m.0"' & pids+=($!)
   done
 done
--- run-fio-seq-read.sh
+++ run-fio-seq-read.sh
@@ -9,9 +9,9 @@ FILE_SIZE=$2
 BLOCK_SIZE=$3
 for count in $(seq 1 3)
 do
-  mkdir -p $1_clients/sequential/read/$count
+  mkdir -p /sts-wire-scale-tests/fio_output/$1_clients/sequential/read/$count
   for ((index=0; index != $(($CLI_NUM * 4)); index=$(($index + 4)))); do
-    wassh -f client-hosts -l root 'CLI_INDEX=$(expr '$(($index + 1))' + $CLIENT_X_FACTOR) && docker exec sts-wire-client$CLI_INDEX sh -c "cd /home/docker/rgw && rm -f seqread.0.0 && fio --name=seqread --rw=read --direct=1 --ioengine=libaio --bs="'$BLOCK_SIZE'" --size="'$FILE_SIZE'" --iodepth=64"' > $1_clients/sequential/read/$count/"sts-wire-client"$(($index + 1))"-seq-read.log" 2>&1 & pids+=($!)
+    wassh -f client-hosts -l root 'CLI_INDEX=$(expr '$(($index + 1))' + $CLIENT_X_FACTOR) && docker exec sts-wire-client$CLI_INDEX sh -c "cd /home/docker/rgw && rm -f seqread.0.0 && fio --name=seqread --rw=read --direct=1 --ioengine=libaio --bs="'$BLOCK_SIZE'" --size="'$FILE_SIZE'" --iodepth=64"' > /sts-wire-scale-tests/fio_output/$1_clients/sequential/read/$count/"sts-wire-client"$(($index + 1))"-seq-read.log" 2>&1 & pids+=($!)
   done
   for pid in "${pids[@]}"; do
     wait "$pid"
